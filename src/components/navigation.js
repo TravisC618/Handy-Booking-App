91a408f4b0d1d404944d1f099ca1d9e7c101174b
travisC <travis_zg@outlook.com>
2020-01-28 15:38:16 +0800

    add navbar demo

---
+++ src/components/navigation.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import logo from '../img/logo.png'
+
+const Navigation = props => {
+    return (
+        <nav className="navbar navbar-expand-md navbar-light sticky-top">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">
+                    <img src={logo} alt="logo" />
+                </a>
+                <form className="form-inline">
+                    <i className="fas fa-search" />
+                    <input className="form-control mr-sm-2" type="search" placeholder="SEARCH BY POSTCODE" aria-label="Search"/>
+                </form>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler">
+                    <span className="navbar-toggler-icon" />
+                </button>
+                <div className="collapse navbar-collapse" id="navbarToggler">
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Browse Handy</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Login/ Register</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navigation;
