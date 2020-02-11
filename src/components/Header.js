d57c692483049b1979b425c870b10fb0936b7c4b
Jinyuan Chen <picowithleo@gmail.com>
2020-02-12 05:35:24 +1000

    add header.js

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../css/header.css';
+
+const Header = props => {
+    return (
+        <div class="header">
+
+       
+            <div class="header__header"> 
+
+            </div>
+
+            <div class="header__body">
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Header;
