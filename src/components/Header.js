269256aa6b30ff15b0c21c1493f7c73d2b0cef08
Jinyuan Chen <picowithleo@gmail.com>
2020-02-12 09:21:37 +1000

    add style elements

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,56 @@
 import React from 'react';
 import '../css/header.css';
+import cleaner from '../img/cleaner.jpg'
+
 
 const Header = props => {
     return (
-        <div class="header">
+        <div classNames="header">
 
-       
-            <div class="header__header"> 
+            <div classNames="header__head"> 
+
+                <div classNames="header__head-bg"></div>
+
+                <button classNames="header__head-btn">
+                    Request a Quote
+                </button>
 
             </div>
 
-            <div class="header__body">
+
+            <div classNames="header__body">
+                <div classNames="header__body-avatar">
+                    <p classNames="header__body-avatar--photo-wrap">
+                        <a>
+                            <img src={cleaner} alt="cleaner's avatar" classNames="header__body-photo"/>
+                        </a>
+                    </p>
+                </div>
+
+                <div classNames="header__body-info">
+                    <h4 classNames="header__body-info-username">Allen M.</h4>
+                    <ul className="header__body-info-userinfo">
+                        <li>
+                            Last online 1 hour ago
+                        </li>
+                        <li >
+                            <i className="fas fa-map-marker-alt" />
+                            Butler WA 6036, Australia
+                        </li>
+                        <li>
+                            Member since 25th Apr 2016
+                        </li>
+                    </ul>
+                    <a> 
+                        <i className="far fa-flag" />
+                        Report this member
+                    </a>
+
+                </div>
+
+                <div classNames="header__body-reviews">
+
+                </div>
 
             </div>
 
