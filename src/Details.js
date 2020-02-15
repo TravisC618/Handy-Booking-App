e952859ec4acb391f565a6d6d1cba5c09ad6e44f
Jinyuan Chen <picowithleo@gmail.com>
2020-02-16 09:33:57 +1000

    modify the detail layout

--- src/Details.js
+++ src/Details.js
@@ -1,10 +1,28 @@
 import React from 'react';
-import Info from './components/Info';
+import Info from './components/details/Info';
+import Reviews from './components/details/Reviews';
+import Sidebar from './components/details/Siderbar';
+import Header from './components/details/Header';
+
 
 const Details = props => {
     return (
-        <div className="details">
-            <Info />
+        <div class="row">
+            <div class="col-sm-12">
+                <div class='text-center'>
+                    <Header />
+                </div>
+
+                <div class="row">
+                    <div class="col-4">
+                        <Sidebar />
+                    </div>
+                    <div class="col-8">
+                        <Info />
+                        <Reviews />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 };
