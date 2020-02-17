d07e4ba04d6d617c7d8f8aa8fa4dec21d89e8aad
Yiming Pan <panyimingau2017@gmail.com>
2020-02-17 12:10:20 +1000

    add task button groups and map

---
+++ src/utils/Switch.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Switch from '@material-ui/core/Switch';
+
+export default function Switches() {
+  const [state, setState] = React.useState({
+    checkedA: true,
+    checkedB: true,
+  });
+
+  const handleChange = name => event => {
+    setState({ ...state, [name]: event.target.checked });
+  };
+
+  return (
+      <Switch
+        checked={state.checkedB}
+        onChange={handleChange('checkedB')}
+        value="checkedB"
+        color="primary"
+        inputProps={{ 'aria-label': 'primary checkbox' }}
+      />
+  );
+}
