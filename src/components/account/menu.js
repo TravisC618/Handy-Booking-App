6951e70af50c5336f5e45b79bbcbbeec980fd550
Jinyuan Chen <picowithleo@gmail.com>
2020-02-28 15:25:25 +1000

    menu page modified

--- src/components/account/menu.js
+++ src/components/account/menu.js
@@ -16,26 +16,26 @@ const Menu = props => {
 
             <div className="menu__list">
                 
-                <div class="menu-folder expanded">
-                    <div class="menu-folder-items showing">
-                        <a class="button" href="/account/dashboard/">Dashboard</a>
-                        <a class="button" href="/account/payment-history/">Payments history</a>
-                        <a class="button">Notifications<span class="number notifications-count off">0</span></a>
+                <div className="menu-folder expanded">
+                    <div className="menu-folder-items showing">
+                        <a className="button" href="/account/dashboard/">Dashboard</a>
+                        <a className="button" href="/account/payment-history/">Payments history</a>
+                        <a className="button">Notifications<span className="number notifications-count off">0</span></a>
                     </div>
                 </div>
-                <div class="menu-folder">
-                    <div class="menu-folder-control showing">
-                        <a class="button">Settings</a>                     
+                <div className="menu-folder">
+                    <div className="menu-folder-control showing">
+                        <a className="button">Settings</a>                     
                     </div>
-                    <div class="menu-folder-items">
-                        <a class="button" href="/account/profile/">Account</a>
-                        <a class="button" href="/account/skills/">Skills</a>
-                        <a class="button" href="/account/badges/">Badges</a>
-                        <a class="button" href="/account/alerts/">Task Alerts</a>
-                        <a class="button" href="/account/notification-settings/">Notification Settings</a>
-                        <a class="button" href="/account/mobile/">Mobile</a>
-                        <a class="button" href="/account/portfolio/">Portfolio</a>
-                        <a class="button" href="/account/password/">Password</a>
+                    <div className="menu-folder-items">
+                        <a className="button" href="/account/profile/">Account</a>
+                        <a className="button" href="/account/skills/">Skills</a>
+                        <a className="button" href="/account/badges/">Badges</a>
+                        <a className="button" href="/account/alerts/">Task Alerts</a>
+                        <a className="button" href="/account/notification-settings/">Notification Settings</a>
+                        <a className="button" href="/account/mobile/">Mobile</a>
+                        <a className="button" href="/account/portfolio/">Portfolio</a>
+                        <a className="button" href="/account/password/">Password</a>
                     </div>
                 </div>
                             
