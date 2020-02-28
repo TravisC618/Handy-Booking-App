2f6e95b765202b9d62525dfc18a3a468f7798684
travisC <travis_zg@outlook.com>
2020-02-28 22:02:54 +0700

    set account components to uppercase & useed url map

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import Navigation from "../components/Navigation";
 import Main from "../components/home/Main";
 import Dashboard from "../components/account/Dashboard";
@@ -9,15 +9,22 @@ import Details from "../Details";
 import BrowseTasks from "../BrowseTasks";
 import Account from "../Account";
 import Menu from "../components/account/Menu";
+import {
+  FIND_CLEANERS_URL,
+  CLEANER_DETAILS_URL,
+  TASK_URL,
+  ACCOUNT_URL
+} from "./URLMAP";
 
 const Routes = () => {
   return (
     <Switch>
       <Route path="/" exact component={Main} />
-      <Route path="/find-cleaners" exact component={FindCleaners} />
-      <Route path="/details" exact component={Details} />
-      <Route path="/tasks" exact component={BrowseTasks} />
-      <Route path="/account/" component={Account} />
+      <Route path={FIND_CLEANERS_URL} exact component={FindCleaners} />
+      <Route path={CLEANER_DETAILS_URL} exact component={Details} />
+      <Route path={TASK_URL} exact component={BrowseTasks} />
+      <Route path={ACCOUNT_URL} component={Account} />
+      <Redirect to="/" /> {/* ALWAYS PUT THIS AT THE BOTTOM */}
     </Switch>
   );
 };
--- src/routes/URLMAP.js
+++ src/routes/URLMAP.js
@@ -1,3 +1,4 @@
 export const FIND_CLEANERS_URL = "/find-cleaners";
 export const CLEANER_DETAILS_URL = "/details";
 export const TASK_URL = "/tasks";
+export const ACCOUNT_URL = "/account";
